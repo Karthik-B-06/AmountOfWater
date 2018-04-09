8cd20d18c830dde8fa27933a4e4c26d5519ed3b0
Karthik B <Karthik.B@mrcooper.com>
2018-04-09 13:41:14 -0500

    Stack based approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,6 @@ import { Component } from '@angular/core';
 import { NgModel, NgForm } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 import { type } from 'os';
-
-
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,11 +11,11 @@ import { type } from 'os';
 
 
 export class AppComponent {
-  
+  sum: number;
+  stack: Array<number> = [];
   arrStr: Array<Object>;
   errormessage: string = "none";
   row: number;
-  title = 'app';
   Arr = Array;
   maxRi: number = 0;
   maxLe: number = 0;
@@ -30,22 +26,24 @@ export class AppComponent {
     this.errormessage = "none";
     var letters = new RegExp('[a-zA-Z]');
     console.log(letters.test(form.value.arrayValues.toString()));
-    
+
     if(letters.test(form.value.arrayValues.toString())) {
       alert('Please enter only Numbers Separated by "," ');
       console.log("false");
     }
     else {
-
+    this.stack = [];
     this.show = true;
     this.arrStr = [];
     this.row = 0;
     this.amount = [];
     this.totalAmount = 0;
     this.amount.push(0);
+    this.sum = 0;
     this.arrStr = form.value.arrayValues.split(",");
     //console.log(this.arrStr[this.arrStr.length-1].toString() + " " + typeof this.arrStr);
     let max = -1;
+
     for(var i=0; i<this.arrStr.length; i++) {
       if( parseInt(this.arrStr[i].toString()) > max ) max = parseInt(this.arrStr[i].toString());
     }
@@ -61,23 +59,43 @@ export class AppComponent {
         this.maxRi = Math.max(this.maxRi,parseInt(this.arrStr[j].toString()));
       }
       this.amount.push(Math.min(this.maxLe,this.maxRi) - parseInt(this.arrStr[i].toString()));
-      this.totalAmount += Math.min(this.maxLe,this.maxRi) - parseInt(this.arrStr[i].toString());
+      this.sum += Math.min(this.maxLe,this.maxRi) - parseInt(this.arrStr[i].toString());
     }
     this.amount.push(0);
-
-    console.log(this.totalAmount);
-    if(isNaN(this.totalAmount)) {
+    
+    if(isNaN(this.sum)) {
       alert("Input Entered Incorrectly");
     }
+    else {
+      this.calculateStackApproach(this.arrStr);
+    }
     console.log(this.amount);
+
+    }
+  }
+
+
+
+  calculateStackApproach(array: Array<Object>) {
+
+    for(var i=0; i<array.length; i++) {
+
+      while(this.stack.length > 0 && parseInt(array[i].toString()) > parseInt(array[this.stack[this.stack.length-1]].toString()) ) {
+        var top = this.stack[this.stack.length-1];
+        this.stack.pop();
+        if(this.stack.length == 0) break;
+
+        var dist = i - this.stack[this.stack.length-1] - 1;
+        var height = Math.min( parseInt(array[i].toString()), parseInt(array[this.stack[this.stack.length-1]].toString()) ) -   parseInt(array[top].toString());
+        this.totalAmount += dist * height;
+      } 
+
+      this.stack.push(i);
+
+
     }
 
 
-
-
-
-
-  }
-
+}
 
 }
